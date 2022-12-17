617e54f991c15e7954811971792ca6176e7483f4
skilalis <skilalis@gmail.com>
2022-12-17 16:25:40 +0600

    Initial commit

---
+++ app/src/main/java/com/skilalis/skilaliswebsite/MainActivity.java
@@ -0,0 +1,39 @@
+package com.skilalis.skilaliswebsite;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import com.example.skilaliswebsite.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webViewId);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://skilalis.com/");
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        if(webView.canGoBack()){
+            webView.goBack();
+        }else {
+            super.onBackPressed();
+        }
+    }
+}
